5ee7e6719adaac9bb2f90f954cac8e076c1dc6be
Shantagouda Kuntoji <sbk@Shantagoudas-MacBook-Pro.local>
2017-10-04 10:02:05 -0700

    Binary Search in Python

---
+++ binarySearch.py
@@ -0,0 +1,19 @@
+def binarySearch(list,n):
+	first = 0
+	last = len(list)-1
+	found = False
+
+	while first<=last and not found:
+		mp = (first+last)//2
+		if list[mp] == n:
+			found =True
+		else:
+			if n < list[mp]:
+				last = mp-1
+			else:
+				first = mp+1
+	return found
+
+list = [1, 4, 6, 10, 15, 17, 20]
+print(binarySearch(list,15)) # True
+print(binarySearch(list, 2)) # False
